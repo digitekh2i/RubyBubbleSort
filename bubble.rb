824213f0ba6f0b4eaf6dced86c2366c62080e2f9
digitekh2i <digitekh2i@gmail.com>
2020-10-14 22:20:03 +0100

    solving bubble sort iteration

--- bubble.rb
+++ bubble.rb
@@ -1,15 +1,17 @@
-def bubble_sort(arr)
-  lengthofArr = arr.length;
-   arr.each_with_index{|item, index|
-   comparedIndex = index + 1
-    comparedItem = arr[comparedIndex]
-    
-    if comparedItem < item {
 
-      temp = arr[comparedItem]
-      arr[comparedIndex] = arr[comparedItem]
-      arr[comparedItem] = temp
-    }
-  }
-puts arr
-end
+def bubble_sort(arr) 
+  lengthofArr = arr.length - 1; 
+  loop do 
+    swap = false 
+    lengthofArr.times do |item|
+       if arr[item] > arr[item + 1] 
+        arr[item], arr[item + 1] = arr[item + 1], arr[item]
+         swap = true 
+        end 
+    end 
+    break if not swap
+  end 
+  puts arr 
+end 
+
+bubble_sort([22, 25, 17, 5, 46, 1]) 
