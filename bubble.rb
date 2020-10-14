d4591a53679650a44930093ff085d218c7dae5f6
Janluc <Janlucsaneaux@gmail.com>
2020-10-14 16:44:53 -0400

    Attempt to solve bubble sort

--- bubble.rb
+++ bubble.rb
@@ -1,1 +1,15 @@
-puts "Hello!"
+def bubble_sort(arr)
+  lengthofArr = arr.length;
+   arr.each_with_index{|item, index|
+   comparedIndex = index + 1
+    comparedItem = arr[comparedIndex]
+    
+    if comparedItem < item {
+
+      temp = arr[comparedItem]
+      arr[comparedIndex] = arr[comparedItem]
+      arr[comparedItem] = temp
+    }
+  }
+puts arr
+end
