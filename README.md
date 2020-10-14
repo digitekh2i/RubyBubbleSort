8859158e4c7172ef46844dcc1091411a6fa58974
digitekh2i <digitekh2i@gmail.com>
2020-10-14 23:45:53 +0100

    updating README file and

---
+++ README.md
@@ -0,0 +1,49 @@
+# Apple Home Page
+
+> For the purpose of educational practical exhibition, In our first ruby project, we applied the Sorting algorithim concept known as Bubble Sort. as part of Microverse Technical curriculum. 
+
+![screenshot]()
+
+
+## Built With
+
+- Ruby,
+- Visual Studio Code, Git and Github.
+
+## Live Demo
+[Live Demo Link]()
+
+## Authors
+
+👤 **Author1**
+
+- Github: [@digitekh2i](https://https://github.com/digitekh2i)
+- Twitter: [@ObokoDaniel](https://twitter.com/ObokoDaniel)
+- Linkedin: [linkedin](http://linkedin.com/in/daniel-dikachi-1luvtek101)
+
+👤 **Author2**
+
+- Github: [@Janluc](https://github.com/Janluc)   
+- Twitter: [@Janluc123](https://twitter.com/Janluc123)
+- Linkedin: [linkedin](https://www.linkedin.com/in/janluc-saneaux-91707a1b4/ 
+
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](lic.url) licensed.
--- bubble.rb
+++ bubble.rb
@@ -1,17 +1,46 @@
 
-def bubble_sort(arr) 
-  lengthofArr = arr.length - 1; 
+def bubble_sort(arr)
+  lengthofArr = arr.length - 1;
   loop do 
-    swap = false 
-    lengthofArr.times do |item|
-       if arr[item] > arr[item + 1] 
-        arr[item], arr[item + 1] = arr[item + 1], arr[item]
-         swap = true 
-        end 
-    end 
-    break if not swap
-  end 
-  puts arr 
-end 
+    swap = false
+     lengthofArr.times do |item|
+        if arr[item] > arr[item + 1] 
+          arr[item], arr[item + 1] = arr[item + 1], arr[item]
+          swap = true
+        end
+      end
+      break if not swap
+  end
+  puts arr   
+end
 
-bubble_sort([22, 25, 17, 5, 46, 1]) 
+
+arr = [49, 22, 1, 55, 104, 7]
+
+bubble_sort(arr)
+
+arr = ["hello", "hey", "hi"]
+
+def bubble_sort_by(arr)
+  lengthofArr = arr.length - 1;
+  loop do 
+    swap = false
+     lengthofArr.times do |item|
+      value = yield(arr[item], arr[item + 1])
+        if value >= 1 
+          arr[item], arr[item + 1] = arr[item + 1], arr[item]
+          swap = true
+        elsif value <= -1
+          break
+        else
+          break
+        end
+      end
+      break if not swap
+  end
+  puts arr
+end
+
+bubble_sort_by(arr) do |left, right|
+  left.length - right.length
+end
